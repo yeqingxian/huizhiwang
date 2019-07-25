66937a18770e8f058c7e2f01bb9ab3ed6bebc958
dongfan93 <407424129@qq.com>
2019-07-25 14:46:23 +0800

    课程

---
+++ huizhi_course_provider_19101/src/main/java/com/edu/huizhi/controller/CourseController.java
@@ -0,0 +1,10 @@
+package com.edu.huizhi.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by dongf on 2019/7/25.
+ */
+@Controller
+public class CourseController {
+}
